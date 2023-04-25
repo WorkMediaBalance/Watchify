eb5b74485a93a2611ead1b925f62a1a788ffc725
Minhyeok Son <mh.blosson@gmail.com>
2023-04-25 15:45:40 +0900

    :sparkles: Feat: Router 기본설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,48 @@
 import React from "react";
 import RecoilTest from "./components/RecoilTest";
 
+import { Routes, Route } from "react-router-dom";
+
+import PageError from "./pages/PageError";
+import PageLogin from "./pages/PageLogin";
+import PageMain from "./pages/PageMain";
+import PageMy from "./pages/PageMy";
+import PageRecommend from "./pages/PageRecommend";
+import PageSchedule from "./pages/PageSchedule";
+import PageSearch from "./pages/PageSearch";
+import PageShare from "./pages/PageShare";
+
+import MemberRoute from "./components/common/MemberRoute";
+import NonMemberRoute from "./components/common/NonMemberRoute";
+
 const App = () => {
   return (
-    <div>
-      <RecoilTest />
-    </div>
+    <>
+      <Routes>
+        {/* 회원, 비회원 모두 접근 가능 */}
+        <Route path="/" element={<PageMain />} />
+        <Route path="/search" element={<PageSearch />} />
+        <Route path="/schedule" element={<PageSchedule />} />
+        <Route path="/recommend" element={<PageRecommend />} />
+        <Route path="/share" element={<PageShare />} />
+
+        {/* 회원만 접근 가능 */}
+        <Route element={<MemberRoute />}>
+          <Route path="/my" element={<PageMy />} />
+        </Route>
+
+        {/* 비회원만 접근 가능 */}
+        <Route element={<NonMemberRoute />}>
+          <Route path="/login" element={<PageLogin />} />
+        </Route>
+
+        {/* 404 페이지 */}
+        <Route path="*" element={<PageError />} />
+      </Routes>
+      <div>
+        <RecoilTest />
+      </div>
+    </>
   );
 };
 
--- frontend/src/pages/PageMain.tsx
+++ frontend/src/pages/PageMain.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const PageMain = () => {
-  return <div></div>;
+  return <div>메인 페이지</div>;
 };
 
 export default PageMain;
