b0f9fa1a2ebf177747878fb3dfc3131a3ce2ca8e
김준형 <gittgi768@gmail.com>
2023-04-28 14:13:04 +0900

    :sparkles: Feat: 탭 바 구현

--- frontend/src/pages/PageMy.tsx
+++ frontend/src/pages/PageMy.tsx
@@ -1,11 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Profile from "components/mypage/Profile";
+import Tabs, { Tab } from "react-best-tabs";
+import "react-best-tabs/dist/index.css";
+
+const StyledTabs = styled(Tabs)`
+  & .rb-tabs-item {
+    width: 25%;
+    // border-top: 0.5px solid ${({ theme }) => theme.netflix.fontColor};
+    border-bottom: 0.5px solid ${({ theme }) => theme.netflix.fontColor};
+    color: ${({ theme }) => theme.netflix.fontColor};
+    font-size: ${({ theme }) => theme.fontSizeType.middle.fontSize} !important;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  & .activeTab {
+    border-bottom: 1px solid ${({ theme }) => theme.netflix.pointColor};
+  }
+
+  & .active {
+    color: ${({ theme }) => theme.netflix.pointColor};
+    font-weight: bold;
+  }
+`;
+const StyledTab = styled(Tab)``;
 
 const PageMy = () => {
+  const [tab, setTab] = useState(1);
   return (
     <div>
       <Profile />
+      <div id="tabs" style={{ marginTop: "2vh" }}>
+        <StyledTabs
+          activeTab={1}
+          className=""
+          ulClassName=""
+          activityClassName="activeTab"
+          onClick={(event, tab) => setTab(tab)}
+        >
+          <StyledTab title="시청 패턴"></StyledTab>
+          <StyledTab title="히스토리"></StyledTab>
+          <StyledTab title="찜 목록"></StyledTab>
+          <StyledTab title="내 정보"></StyledTab>
+        </StyledTabs>
+      </div>
     </div>
   );
 };
