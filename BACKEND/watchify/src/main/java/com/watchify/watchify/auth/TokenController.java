69f1cecb7095a581994cf5104cf2c6add77a68a3
고병진 <gotommao@gmail.com>
2023-05-04 18:03:42 +0900

    :sparkles: Feat: OAuth2 401 Unauthorized: no body 에러..

--- BACKEND/watchify/src/main/java/com/watchify/watchify/auth/TokenController.java
+++ BACKEND/watchify/src/main/java/com/watchify/watchify/auth/TokenController.java
@@ -29,29 +29,27 @@ public class TokenController {
         RestTemplate restTemplate = new RestTemplate();
 
         HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        headers.add("Accept", "application/json");
 
         // body 데이터를 담을 오브젝트인 MultiValueMap를 만들어보자
         // body는 보통 key, value의 쌍으로 이루어지기 때문에 자바에서 제공해주는 MultiValueMap 타입을 사용한다.
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         params.add("grant_type", "authorization_code");
-        params.add("client_id", "e054053c94984f87d0f21b0abe71d10e");
-        params.add("redirect_uri", "http://localhost:8080/auth/kakao/callback");
+        params.add("client_id", "****");
+        params.add("redirect_uri", "****");
         params.add("code", code);
 
         // 요청하기 위해 헤더(Header)와 데이터(Body)를 합친다.
         // kakaoTokenRequest는 데이터(Body)와 헤더(Header)를 Entity가 된다.
-        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(params, headers);
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
+        System.out.println(request);
 
         // POST 방식으로 Http 요청한다. 그리고 response 변수의 응답 받는다.
-        ResponseEntity<String> response = restTemplate.exchange(
-                "https://kauth.kakao.com/oauth/token", // https://{요청할 서버 주소}
-                HttpMethod.POST, // 요청할 방식
-                kakaoTokenRequest, // 요청할 때 보낼 데이터
-                String.class // 요청 시 반환되는 데이터 타입
-        );
+        ResponseEntity<String> stringResponseEntity = restTemplate.postForEntity(REQUEST_URL, request, String.class);
 
-        return "카카오 인증 완료 : 토큰 요청 응답 : " + response;
+        System.out.println("1번");
+        return "카카오 인증 완료 : 토큰 요청 응답 : ";
     }
 
 
