3705afb10854fa1c64932fb3661f38ef264779ab
김준형 <gittgi768@gmail.com>
2023-05-04 17:40:16 +0900

    :sparkles: Feat: 메인페이지 제작 중

---
+++ frontend/src/components/main/TodayWatch.tsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import styled, { keyframes, css } from "styled-components";
+
+const TodayWatch = () => {
+  const [clickState, setClickState] = useState(0);
+  const handleState = (index: number) => {
+    setClickState(index);
+  };
+
+  return (
+    <Container
+      onClick={() => {
+        console.log(clickState);
+
+        handleState(1);
+      }}
+    >
+      <BottomRectangle
+        onClick={(e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+          e.stopPropagation();
+          console.log(clickState);
+          handleState(2);
+        }}
+        clickState={clickState}
+      ></BottomRectangle>
+    </Container>
+  );
+};
+
+export default TodayWatch;
+
+const Container = styled.div`
+  position: relative;
+  width: 100vw;
+  height: 30vh;
+  background-color: red;
+  // overflow: hidden;
+`;
+
+const state0to1 = keyframes`
+  from {
+    clip-path: polygon(0% 70%, 100% 30%, 100% 100%, 0% 100%);
+  }
+  
+  to {
+    clip-path: polygon(100% 70%, 100% 100%, 25% 100%);
+  }
+`;
+
+const state1to2 = keyframes`
+  from {
+    clip-path: polygon(0% 70%, 100% 30%, 100% 100%, 0% 100%);
+  }
+  
+  to {
+    clip-path: polygon(0% 30%, 75% 0%, 100% 0%, 100% 30%, 100% 100%, 0% 100%);
+  }
+`;
+
+const BottomRectangle = styled.div<{ clickState: number }>`
+  height: 30vh;
+  width: 100vw;
+  background-color: green;
+  clip-path: polygon(0% 70%, 100% 30%, 100% 100%, 0% 100%);
+  animation: ${(props) => {
+    if (props.clickState === 1) {
+      return css`
+        ${state0to1} 1s forwards;
+      `;
+    } else if (props.clickState === 2) {
+      return css`
+        ${state1to2} 1s forwards;
+      `;
+    }
+  }};
+`;
--- frontend/src/pages/PageMain.tsx
+++ frontend/src/pages/PageMain.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import Carousel from "../components/common/Carousel";
+import TodayWatch from "components/main/TodayWatch";
 
 const PageMain = () => {
   return (
     <div>
-      <p>메인 페이지</p>
+      <TodayWatch></TodayWatch>
+      {/* <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "auto",
+          height: "30vh",
+          background: `url(https://images.justwatch.com/backdrop/302937718/s1440/mobeomtaegsi.webp) no-repeat center center`,
+          backgroundSize: "cover",
+          marginBottom: "3vh",
+        }}
+      ></div> */}
       <Carousel />
     </div>
   );
