59884f6443557e47efaf0449f5f3763678bb3e09
김준형 <gittgi768@gmail.com>
2023-05-09 09:59:46 +0900

    :bug: Fix: 바텀시트 모션 개선

--- frontend/src/recoil/recommendState.ts
+++ frontend/src/recoil/recommendState.ts
@@ -21,7 +21,8 @@ export const recResultState = atom<Array<content>>({
       runtime: 50,
       rate: 4.5,
       img_path: "https://images.justwatch.com/poster/302518136/s592/시즌-1",
-      backdrop_path: "https://images.justwatch.com/poster/302518136/s592/시즌-1",
+      backdrop_path:
+        "https://images.justwatch.com/poster/302518136/s592/시즌-1",
       isLike: 1,
       type: "드라마",
       season: 1,
@@ -29,7 +30,7 @@ export const recResultState = atom<Array<content>>({
       ott: ["netflix"],
       genres: ["드라마"],
       isWish: false,
-      isLike: 1,
+
       summarize:
         "어느 날 길을 걷던 걸무고는 우연히 알 수 없는 동전을 줍게 되고, 이 동전의 충격적인 정체가 알려지며 사건에 휩싸이게 되는데... 과연 걸무고는 무사할 수 있을까?",
       audienceAge: 15,
@@ -39,8 +40,10 @@ export const recResultState = atom<Array<content>>({
       title: "트와일라잇",
       runtime: 60,
       rate: 3.7,
-      img_path: "https://images.justwatch.com/poster/129382738/s592/twilight.webp",
-      backdrop_path: "https://images.justwatch.com/poster/302518136/s592/시즌-1",
+      img_path:
+        "https://images.justwatch.com/poster/129382738/s592/twilight.webp",
+      backdrop_path:
+        "https://images.justwatch.com/poster/302518136/s592/시즌-1",
       isLike: 1,
       type: "영화",
       season: 0,
@@ -48,7 +51,7 @@ export const recResultState = atom<Array<content>>({
       ott: ["wavve", "watcha"],
       genres: ["야생", "뱀파이어"],
       isWish: true,
-      isLike: -1,
+
       summarize: "뱀파이어가 울부지저따. 뱀파이어는 짱 쎄따. 크와아앙",
       audienceAge: 15,
     },
@@ -58,8 +61,10 @@ export const recResultState = atom<Array<content>>({
         "고병진이 살아있다 : Walking Moving Byeongjin 고병진이 살아있다 : Walking Moving Byeongjin",
       runtime: 88,
       rate: 1.2,
-      img_path: "https://images.justwatch.com/poster/8733916/s592/bagmulgwani-salaissda.webp",
-      backdrop_path: "https://images.justwatch.com/poster/302518136/s592/시즌-1",
+      img_path:
+        "https://images.justwatch.com/poster/8733916/s592/bagmulgwani-salaissda.webp",
+      backdrop_path:
+        "https://images.justwatch.com/poster/302518136/s592/시즌-1",
       isLike: 1,
       type: "영화",
       season: 0,
@@ -67,7 +72,7 @@ export const recResultState = atom<Array<content>>({
       ott: ["wavve", "netflix", "disney"],
       genres: ["박물관", "리얼리티"],
       isWish: false,
-      isLike: 0,
+
       summarize:
         "한참 어린 동생으로부터 스타 도전을 받은 걸무고, 과연 그는 저그의 자존심을 지켜낼 수 있을 것인가? 5월 1일. Python 개봉 박두",
       audienceAge: 15,
@@ -78,7 +83,8 @@ export const recResultState = atom<Array<content>>({
       runtime: 8,
       rate: 1.2,
       img_path: "https://images.justwatch.com/poster/302518136/s592/시즌-1",
-      backdrop_path: "https://images.justwatch.com/poster/302518136/s592/시즌-1",
+      backdrop_path:
+        "https://images.justwatch.com/poster/302518136/s592/시즌-1",
       isLike: 1,
       type: "영화",
       season: 1,
@@ -86,7 +92,7 @@ export const recResultState = atom<Array<content>>({
       ott: ["wavve"],
       genres: ["박물관", "리얼리티"],
       isWish: false,
-      isLike: 0,
+
       summarize:
         "한참 어린 동생으로부터 스타 도전을 받은 걸무고, 과연 그는 저그의 자존심을 지켜낼 수 있을 것인가? 5월 1일. Python 개봉 박두",
       audienceAge: 15,
@@ -97,7 +103,8 @@ export const recResultState = atom<Array<content>>({
       runtime: 8,
       rate: 1.2,
       img_path: "https://images.justwatch.com/poster/302518136/s592/시즌-1",
-      backdrop_path: "https://images.justwatch.com/poster/302518136/s592/시즌-1",
+      backdrop_path:
+        "https://images.justwatch.com/poster/302518136/s592/시즌-1",
       isLike: 1,
       type: "영화",
       season: 1,
@@ -105,7 +112,7 @@ export const recResultState = atom<Array<content>>({
       ott: ["wavve"],
       genres: ["박물관", "리얼리티"],
       isWish: false,
-      isLike: 0,
+
       summarize:
         "한참 어린 동생으로부터 스타 도전을 받은 걸무고, 과연 그는 저그의 자존심을 지켜낼 수 있을 것인가? 5월 1일. Python 개봉 박두",
       audienceAge: 15,
@@ -116,7 +123,8 @@ export const recResultState = atom<Array<content>>({
       runtime: 8,
       rate: 1.2,
       img_path: "https://images.justwatch.com/poster/302518136/s592/시즌-1",
-      backdrop_path: "https://images.justwatch.com/poster/302518136/s592/시즌-1",
+      backdrop_path:
+        "https://images.justwatch.com/poster/302518136/s592/시즌-1",
       isLike: 1,
       type: "영화",
       season: 1,
@@ -124,7 +132,7 @@ export const recResultState = atom<Array<content>>({
       ott: ["wavve"],
       genres: ["박물관", "리얼리티"],
       isWish: false,
-      isLike: 0,
+
       summarize:
         "한참 어린 동생으로부터 스타 도전을 받은 걸무고, 과연 그는 저그의 자존심을 지켜낼 수 있을 것인가? 5월 1일. Python 개봉 박두",
       audienceAge: 15,
@@ -135,7 +143,8 @@ export const recResultState = atom<Array<content>>({
       runtime: 8,
       rate: 1.2,
       img_path: "https://images.justwatch.com/poster/302518136/s592/시즌-1",
-      backdrop_path: "https://images.justwatch.com/poster/302518136/s592/시즌-1",
+      backdrop_path:
+        "https://images.justwatch.com/poster/302518136/s592/시즌-1",
       isLike: 1,
       type: "영화",
       season: 1,
@@ -143,7 +152,7 @@ export const recResultState = atom<Array<content>>({
       ott: ["wavve"],
       genres: ["박물관", "리얼리티"],
       isWish: false,
-      isLike: 0,
+
       summarize:
         "한참 어린 동생으로부터 스타 도전을 받은 걸무고, 과연 그는 저그의 자존심을 지켜낼 수 있을 것인가? 5월 1일. Python 개봉 박두",
       audienceAge: 15,
@@ -154,7 +163,8 @@ export const recResultState = atom<Array<content>>({
       runtime: 8,
       rate: 1.2,
       img_path: "https://images.justwatch.com/poster/302518136/s592/시즌-1",
-      backdrop_path: "https://images.justwatch.com/poster/302518136/s592/시즌-1",
+      backdrop_path:
+        "https://images.justwatch.com/poster/302518136/s592/시즌-1",
       isLike: 1,
       type: "영화",
       season: 1,
@@ -162,7 +172,7 @@ export const recResultState = atom<Array<content>>({
       ott: ["wavve"],
       genres: ["박물관", "리얼리티"],
       isWish: false,
-      isLike: 0,
+
       summarize:
         "한참 어린 동생으로부터 스타 도전을 받은 걸무고, 과연 그는 저그의 자존심을 지켜낼 수 있을 것인가? 5월 1일. Python 개봉 박두",
       audienceAge: 15,
