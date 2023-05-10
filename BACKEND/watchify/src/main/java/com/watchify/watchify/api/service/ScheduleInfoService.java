40de681a5ecb24200aa80cc96f01e723b4d1a960
고병진 <gotommao@gmail.com>
2023-05-10 16:25:50 +0900

    :rocket: Chore: scheduleInfoAPI 개발중

--- BACKEND/watchify/src/main/java/com/watchify/watchify/api/service/ScheduleInfoService.java
+++ BACKEND/watchify/src/main/java/com/watchify/watchify/api/service/ScheduleInfoService.java
@@ -1,10 +1,14 @@
 package com.watchify.watchify.api.service;
 
+import com.watchify.watchify.db.entity.Calender;
+import com.watchify.watchify.db.repository.CalenderRepository;
 import com.watchify.watchify.dto.response.CalenderDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
@@ -13,10 +17,28 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class ScheduleInfoService {
 
-    private Cla
+    private CalenderRepository calenderRepository;
 
     @Transactional
     public Map<Integer, List<CalenderDTO>> getScheduleInfo(Long userId, int year, int month) {
+
+        LocalDate startDate = LocalDate.of(year, month, 1);
+        LocalDate endDate = startDate.withDayOfMonth(startDate.lengthOfMonth());
+
+        // 그달에 해당하는 캘린더 값들
+        List<Calender> monthOfCalenders = calenderRepository.getSchedule(userId, startDate, endDate);
+        monthOfCalenders.sort(Comparator.comparing(Calender :: getDate)); // 날짜순으로 정렬
+
+        for (Calender c : monthOfCalenders) {
+            System.out.println(c.getDate());
+        }
+
+        Map<Integer, List<CalenderDTO>> a = null;
+        return a;
+
+
+
+
 
     }
 
