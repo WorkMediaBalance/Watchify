aaf4b7f51270c4415c88b220333f8af768e76201
김준형 <gittgi768@gmail.com>
2023-05-10 17:52:55 +0900

    :bug: Fix: 각 날짜 크기 동일하게 100%로 맞추

--- frontend/src/components/schedule/calendar/Calendar.tsx
+++ frontend/src/components/schedule/calendar/Calendar.tsx
@@ -319,8 +319,10 @@ const SP = styled.div`
 const InnerConteiner = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: start;
   width: 100%;
+  height: 100%;
+  margin-top: 0.2vh;
 `;
 
 const ContentTag = styled.div`
