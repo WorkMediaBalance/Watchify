b37e6761b92b448e6e87a3f615c91bd616e17604
고병진 <gotommao@gmail.com>
2023-05-10 17:58:51 +0900

    :sparkles: Feat: 컨텐츠 찜 API 개발, 테스트 필요

--- BACKEND/watchify/src/main/java/com/watchify/watchify/db/repository/WishContentRepository.java
+++ BACKEND/watchify/src/main/java/com/watchify/watchify/db/repository/WishContentRepository.java
@@ -14,4 +14,7 @@ public interface WishContentRepository extends JpaRepository<WishContent, Long> 
 
     @Query(value = "select * from wish_content wc where wc.user_id = :userId and is_deleted = false order by wc.updated_at desc ", nativeQuery = true)
     List<WishContent> getMyWishList(@Param("userId") Long userId);
+
+    @Query(value = "select * from wish_content wc where wc.user_id = :userId order by wc.updated_at desc ", nativeQuery = true)
+    List<WishContent> getAllMyWishList(@Param("userId") Long userId);
 }
