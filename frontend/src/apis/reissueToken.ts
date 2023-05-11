ad25e4bfa591e0f33533d4e493a55169e5f73e53
Minhyeok Son <mh.blosson@gmail.com>
2023-05-11 13:12:32 +0900

    :sparkles: Feat: signin -> login 코드 수정

--- frontend/src/apis/reissueToken.ts
+++ frontend/src/apis/reissueToken.ts
@@ -15,28 +15,27 @@ const refreshAxios = axios.create({
 });
 
 export const reissueAccessToken = async () => {
-  refreshAxios.defaults.headers["Authorization"] =
-    "Bearer " + localStorage.getItem("refreshToken");
+  refreshAxios.defaults.headers["Authorization"] = "Bearer " + localStorage.getItem("refreshToken");
   await refreshAxios
     .get("/api/auth/refresh")
     .then((response) => {
       localStorage.setItem("accessToken", response.data);
     })
     .catch((error) => {
-      localStorage.clear();
-      Swal.fire({
-        title: "세션이 만료되었습니다.",
-        text: "다시 로그인해주세요",
-        icon: "error",
-        timer: 1500,
-        customClass: {
-          container: "my-swal-container",
-          confirmButton: "my-swal-confirm-button",
-          cancelButton: "my-swal-cancel-button",
-          icon: "my-swal-icon",
-        },
-      }).then((result) => {
-        window.location.href = `${BASE_URL}/signin`;
-      });
+      // localStorage.clear();
+      // Swal.fire({
+      //   title: "세션이 만료되었습니다.",
+      //   text: "다시 로그인해주세요",
+      //   icon: "error",
+      //   timer: 1500,
+      //   customClass: {
+      //     container: "my-swal-container",
+      //     confirmButton: "my-swal-confirm-button",
+      //     cancelButton: "my-swal-cancel-button",
+      //     icon: "my-swal-icon",
+      //   },
+      // }).then((result) => {
+      //   window.location.href = `${BASE_URL}/login`;
+      // });
     });
 };
